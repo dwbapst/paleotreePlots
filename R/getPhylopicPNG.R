0bda99e40002b2dac553fca2ada12afb0b60b9eb
David Bapst <dwbapst@gmail.com>
2019-01-30 09:07:14 -0600

    collapsing scott's functions

--- R/getPhylopicPNG.R
+++ R/getPhylopicPNG.R
@@ -1,8 +1,17 @@
 # raw innards from Scott Chamberlain's rphylopic package
 
+
+
+
 image_get <- function(uuid, options=NULL, ...) {
   args <- if (!is.null(options)) list(options = paste0(options, collapse = " ")) else list()
-  phy_GET(file.path('api/a/image', uuid), args, ...)$result
+  path <- file.path('api/a/image', uuid)
+  cli <- crul::HttpClient$new(url = pbase(), opts = list(...))
+  tt <- cli$get(path = path, query = as_null(pc(args)))
+  tt$raise_for_status()
+  tmp <- tt$parse("UTF-8")
+  x <- jsonlite::fromJSON(tmp, FALSE)
+  x$result
 }
 
 
@@ -13,16 +22,15 @@ get_png <- function(x, ...) {
 }
 
 phy_GET <- function(path, args, ...) {
-  tmp <- phy_GET2(path, args, ...)
-  jsonlite::fromJSON(tmp, FALSE)
+
 }
 
 phy_GET2 <- function(path, args, ...) {
-  cli <- crul::HttpClient$new(url = pbase(), opts = list(...))
-  tt <- cli$get(path = path, query = as_null(pc(args)))
-  tt$raise_for_status()
-  tt$parse("UTF-8")
+
 }
 
 ibase <- function() "http://phylopic.org/api/a/image/"
 pbase <- function() "http://phylopic.org"
+
+
+
