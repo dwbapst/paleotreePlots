f6854730a07461af3e63415af6211eefcf4891ec
David Bapst <dwbapst@gmail.com>
2019-02-06 09:27:58 -0600

    Update getPhylopicPNG.R

--- R/getPhylopicPNG.R
+++ R/getPhylopicPNG.R
@@ -1,9 +1,9 @@
 # raw innards from Scott Chamberlain's rphylopic package
 
 
-
-
 image_get <- function(uuid, options=NULL, ...) {
+  
+  
   args <- if (!is.null(options)) list(options = paste0(options, collapse = " ")) else list()
   path <- file.path('api/a/image', uuid)
   cli <- crul::HttpClient$new(url = pbase(), opts = list(...))
@@ -12,14 +12,18 @@ image_get <- function(uuid, options=NULL, ...) {
   tmp <- tt$parse("UTF-8")
   x <- jsonlite::fromJSON(tmp, FALSE)
   x$result
-}
+
+  
+  }
 
 
 get_png <- function(x, ...) {
+
   tmp <- crul::HttpClient$new(url = pbase(), opts = list(...))
   res <- tmp$get(x)
   png::readPNG(res$content)
-}
+
+  }
 
 phy_GET <- function(path, args, ...) {
 
@@ -38,3 +42,6 @@ foxurl<-"http://phylopic.org/assets/images/submissions/51b1b6e4-129d-41a6-bbbd-c
 raburl<-"http://phylopic.org/assets/images/submissions/1e15411c-5394-4a9d-a209-76c8ac0c331d.1024.png"
 fox_logo <-  readPNG(getURLContent(foxurl))
 rab_logo <-  readPNG(getURLContent(raburl))
+
+
+
